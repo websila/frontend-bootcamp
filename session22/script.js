e3b5bb9996bb7b276f19470ebfed22e6004ab5e1
Alireza Mohammadi <websila.ir@gmail.com>
2022-10-12 10:20:53 +0330

    first commit

---
+++ session22/script.js
@@ -0,0 +1,23 @@
+/**
+ * ! DOM Traversing
+ * ? Downwards :
+ * * querySelectorAll , children
+ */
+
+/**
+ * ? Upwards :
+ * * parentElement , closet
+ */
+
+/**
+ * ? Sideways :
+ * * nextElementSibling , previousElementSibling , Combining parentElement, children
+ */
+
+/**
+ * ! async - await
+ */
+
+/**
+ * ! module import / export
+ */
