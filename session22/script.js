08318a51e27d2ea6718f106956e09d96a0dd9f1c
Alireza Mohammadi <websila.ir@gmail.com>
2022-10-13 20:37:57 +0330

    session 22 updated

--- session22/script.js
+++ session22/script.js
@@ -1,6 +1,6 @@
 // ! destructing arrays
-const numbers = [1, 5, 6, "et", "dsgs"];
-/* const a = numbers[0];
+/*const numbers = [1, 5, 6, "et", "dsgs"];
+const a = numbers[0];
 const b = numbers[1];
 const c = numbers[2];
 const d = numbers[3];
@@ -25,47 +25,65 @@ console.log(id, title, content, author, tags); */
 // const { id: x, tags: barchasb } = post1;
 
 // ! speard operator (...)
-const users1 = ["Brant", "Thaddeus", "Edison", "Effie"];
-// console.log(users1);
-// console.log(...users1);
-// const newUser = prompt("نام کاربر را وارد نمایید");
-// const users2 = [...users1, newUser];
-// console.log(users2);
+/* const users1 = ["Brant", "Thaddeus", "Edison", "Effie"];
+console.log(users1);
+console.log(...users1);
+const newUser = prompt("نام کاربر را وارد نمایید");
+const users2 = [...users1, newUser];
+console.log(users2);
 const logger = function (a, b, c) {
   console.log(a, b, c);
 };
 const inputs = ["test1", "test2", "test3"];
-logger(...inputs);
+logger(...inputs); */
 
 // ! DOM Traversing
 // ? DOM Traversing : Downwards
 // * querySelectorAll , children
+/* const select1 = document.querySelectorAll("li");
+const select2 = document.getElementById("inner-list").querySelectorAll("li");
+const select3 = document.getElementById("inner-list").children;
+console.log(select1);
+console.log(select2);
+console.log(select3); */
 
 // ? DOM Traversing : Upwards
-// * parentElement , closet
+// * parentElement , closest
+/* const select1 = document.getElementById("inner-list").parentElement;
+const select2 = document.querySelector("#inner-list li");
+const select3 = select2.closest("ul"); */
 
 // ? DOM Traversing : Sideways
 // * nextElementSibling , previousElementSibling , Combining parentElement, children
+/* let select1 = document.querySelector("#inner-list li");
+const select2 = select1.nextElementSibling;
+const select3 = select1.previousElementSibling;
+const select4 = [];
+while (select1.nextElementSibling) {
+  select4.push(select1.nextElementSibling);
+  select1 = select1.nextElementSibling;
+}
+console.log(select4); */
 
 // ! apply() , bind() , call()
-/* const car1 = {
+
+const car1 = {
   model: "Mercedes-Benz E-Class",
   maxSpeed: "250 kmph",
 };
 const logger = function (a, b) {
-  console.log(a, b);
-  console.log(this);
+  console.log("ab", a, b);
+  console.log("this", this);
 };
-logger("text1", "text2");
+//logger("text1", "text2");
 
 // * apply()
 logger.apply(car1, ["text1", "text2"]);
-
 // * call()
 logger.call(car1, "text1", "text2");
 
 // * bind()
-logger.bind(car1, "text1", "text2")(); */
+logger.bind("testsss", "text1", "text2")();
 
 // ! setTimeOut()
 
